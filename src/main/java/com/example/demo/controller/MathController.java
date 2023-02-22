8db31105e710ac04413ed113d87e98ec25bb61ba
chihiro.nakamura <chihiro.moment.1127@gmail.com>
2023-02-23 05:01:00 +0900

    create addition

--- src/main/java/com/example/demo/controller/MathController.java
+++ src/main/java/com/example/demo/controller/MathController.java
@@ -3,79 +3,86 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class MathController {		
-	String l = "1１";
-	String r = "2";
-	
-	@GetMapping("/math")
-	public String param( Model model ) {		
-		char bufA[] = l.toCharArray();
-		char bufB[] = r.toCharArray();
+public class MathController {
+	@GetMapping( "/math" )
+	public String math() {
+		return "math.html";
+	}
+
+	@PostMapping( value="/form" )	
+	public String math( @RequestParam( "str1" ) String str1, @RequestParam( "str2" ) String str2, Model model ) {		
+		char bufA[] = str1.toCharArray();
+		char bufB[] = str2.toCharArray();
 		
 		// 数字判定
-		if ( checkNumber( bufA ) == true && checkNumber( bufB ) == true ) {
-			System.out.println( "true" );
+		try {
+			if ( checkNumber( bufA ) == true && checkNumber( bufB ) == true ) {
+	
+				String num1 = toHalfWidth( str1 );
+				String num2 = toHalfWidth( str2 );
 
-			System.out.println( l = toHalfWidth( l ) );
-
-			result( model );
-//			// 全角か半角か
-//			if ( checkWidth( l ) == true && checkWidth( r ) == true ) {
-//				System.out.println( "true" );
-//				
-//				System.out.println(l);
-//				// 小数混入かどうか
-//			
-//				result( model );
-//			} else {
-//				toHalfWidth( l );
-//			}
-		} else {
-			errNumber( model );
+				if ( checkInt( num1 ) == true && checkInt( num2 ) == true ) {
+					int math = Integer.parseInt( num1 ) + Integer.parseInt( num2 );
+					model.addAttribute( "result", math );
+					return "result.html";
+				} else if ( checkInt( num1 ) == true && checkInt( num2 ) == false ) {
+					double math = Integer.parseInt( num2 ) + Double.parseDouble( num2 );
+					model.addAttribute( "result", math );
+					return "result.html";
+				} else if ( checkInt( num1 ) == false && checkInt( num2 ) == false ) {
+					double math = Double.parseDouble( num1 ) + Integer.parseInt( num2 );
+					model.addAttribute( "result", math );
+					return "result.html";
+				} else {
+					double math = Double.parseDouble( num1 ) + Double.parseDouble( num2 );
+					model.addAttribute( "result", math );
+					return "result.html";
+				}
+			} else {
+				model.addAttribute( "result", "数字を入力して下さい。" );
+				return "result.html";
+			}
+		} catch( NumberFormatException e ) {
+			model.addAttribute( "result", "数字を入力して下さい。" );
+			return "result.html";
 		}
-		return null;
 	}
 
 	// 数字判定
 	public static boolean checkNumber(char char1[]) {
 		for (char ch : char1 ) {
-            //数字かどうかを判定
             if ( Character.isDigit( ch )) {
-            	// 整数か小数に変更する
                 return true;
             } else {
                 return false;
             }
 		}
 		return false;
-
 	}
 
 	// 全角→半角変換
-	public String toHalfWidth(String s) {
-	  StringBuilder sb = new StringBuilder(s);
-	  for (int i = 0; i < s.length(); i++) {
-	    char c = s.charAt(i);
-	    if (0xFF10 <= c && c <= 0xFF19) {
-	      sb.setCharAt(i, (char) (c - 0xFEE0));
+	public String toHalfWidth( String s ) {
+		StringBuilder sb = new StringBuilder( s );
+
+		for ( int i = 0; i < s.length(); i++ ) {
+			char c = s.charAt( i );
+			if ( 0xFF10 <= c && c <= 0xFF19 ) {
+				sb.setCharAt( i, ( char ) ( c - 0xFEE0 ));
+			}
+		}
+		return sb.toString();
+	}
+
+	public static boolean checkInt( String s ) {
+	    try {
+	    	Integer.parseInt( s );
+	    	return true;
+	    } catch ( NumberFormatException e ) {
+	    	return false;
 	    }
-	  }
-	  return sb.toString();
-	}
-	
-	// 成功文
-	public String result( Model model ) {
-		model.addAttribute( "result", l + r );
-
-		return "math.html";
-	}
-
-	// 数字エラー
-	public String errNumber( Model model1 ) {
-		model1.addAttribute( "result", "数字を入力して下さい。" );
-
-		return "math.html";
 	}
 }
