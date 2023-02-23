8f9c711ef31d415e1f03a3f704a0f6b4a58776e3
chihiro.nakamura <chihiro.moment.1127@gmail.com>
2023-02-23 16:15:57 +0900

    update math

--- src/main/java/com/example/demo/controller/MathController.java
+++ src/main/java/com/example/demo/controller/MathController.java
@@ -13,42 +13,97 @@ public class MathController {
 		return "math.html";
 	}
 
-	@PostMapping( value="/form" )	
-	public String math( @RequestParam( "str1" ) String str1, @RequestParam( "str2" ) String str2, Model model ) {		
-		char bufA[] = str1.toCharArray();
-		char bufB[] = str2.toCharArray();
-		
-		// 数字判定
-		try {
-			if ( checkNumber( bufA ) == true && checkNumber( bufB ) == true ) {
-	
-				String num1 = toHalfWidth( str1 );
-				String num2 = toHalfWidth( str2 );
+	@PostMapping( value="/form", params="plus" )	
+	public String plus( @RequestParam( "str1" ) String str1, @RequestParam( "str2" ) String str2, Model model ) {
+		// 桁数指定
+		if ( str1.length() > 9 || str2.length() > 9 ) {
+			model.addAttribute( "result", "計算結果が10桁を超える計算はできません" );
+			return "result.html";
+		} else {
+			char bufA[] = str1.toCharArray();
+			char bufB[] = str2.toCharArray();
 
-				if ( checkInt( num1 ) == true && checkInt( num2 ) == true ) {
-					int math = Integer.parseInt( num1 ) + Integer.parseInt( num2 );
-					model.addAttribute( "result", math );
-					return "result.html";
-				} else if ( checkInt( num1 ) == true && checkInt( num2 ) == false ) {
-					double math = Integer.parseInt( num2 ) + Double.parseDouble( num2 );
-					model.addAttribute( "result", math );
-					return "result.html";
-				} else if ( checkInt( num1 ) == false && checkInt( num2 ) == false ) {
-					double math = Double.parseDouble( num1 ) + Integer.parseInt( num2 );
-					model.addAttribute( "result", math );
-					return "result.html";
+			try {
+				// 数字判定
+				if ( checkNumber( bufA ) == true && checkNumber( bufB ) == true ) {
+
+					// 半角数字に変換
+					String num1 = toHalfWidth( str1 );
+					String num2 = toHalfWidth( str2 );
+
+					// 整数か小数判定
+					if ( checkInt( num1 ) == true && checkInt( num2 ) == true ) {
+						int math = Integer.parseInt( num1 ) + Integer.parseInt( num2 );
+						model.addAttribute( "result", math );
+						return "result.html";
+					} else if ( checkInt( num1 ) == true && checkInt( num2 ) == false ) {
+						double math = Integer.parseInt( num2 ) + Double.parseDouble( num2 );
+						model.addAttribute( "result", math );
+						return "result.html";
+					} else if ( checkInt( num1 ) == false && checkInt( num2 ) == true ) {
+						double math = Double.parseDouble( num1 ) + Integer.parseInt( num2 );
+						model.addAttribute( "result", math );
+						return "result.html";
+					} else {
+						double math = Double.parseDouble( num1 ) + Double.parseDouble( num2 );
+						model.addAttribute( "result", math );
+						return "result.html";
+					}
 				} else {
-					double math = Double.parseDouble( num1 ) + Double.parseDouble( num2 );
-					model.addAttribute( "result", math );
+					model.addAttribute( "result", "数字を入力して下さい。" );
 					return "result.html";
 				}
-			} else {
+			} catch( NumberFormatException e ) {
 				model.addAttribute( "result", "数字を入力して下さい。" );
 				return "result.html";
 			}
-		} catch( NumberFormatException e ) {
-			model.addAttribute( "result", "数字を入力して下さい。" );
+		}
+	}
+	
+	@PostMapping( value="/form", params="minus" )	
+	public String minus( @RequestParam( "str1" ) String str1, @RequestParam( "str2" ) String str2, Model model ) {
+		// 桁数指定
+		if ( str1.length() > 9 || str2.length() > 9 ) {
+			model.addAttribute( "result", "計算結果が10桁を超える計算はできません" );
 			return "result.html";
+		} else {
+			char bufA[] = str1.toCharArray();
+			char bufB[] = str2.toCharArray();
+
+			try {
+				// 数字判定
+				if ( checkNumber( bufA ) == true && checkNumber( bufB ) == true ) {
+
+					// 半角数字に変換
+					String num1 = toHalfWidth( str1 );
+					String num2 = toHalfWidth( str2 );
+
+					// 整数か小数判定
+					if ( checkInt( num1 ) == true && checkInt( num2 ) == true ) {
+						int math = Integer.parseInt( num1 ) - Integer.parseInt( num2 );
+						model.addAttribute( "result", math );
+						return "result.html";
+					} else if ( checkInt( num1 ) == true && checkInt( num2 ) == false ) {
+						double math = Integer.parseInt( num2 ) - Double.parseDouble( num2 );
+						model.addAttribute( "result", math );
+						return "result.html";
+					} else if ( checkInt( num1 ) == false && checkInt( num2 ) == true ) {
+						double math = Double.parseDouble( num1 ) - Integer.parseInt( num2 );
+						model.addAttribute( "result", math );
+						return "result.html";
+					} else {
+						double math = Double.parseDouble( num1 ) - Double.parseDouble( num2 );
+						model.addAttribute( "result", math );
+						return "result.html";
+					}
+				} else {
+					model.addAttribute( "result", "数字を入力して下さい。" );
+					return "result.html";
+				}
+			} catch( NumberFormatException e ) {
+				model.addAttribute( "result", "数字を入力して下さい。" );
+				return "result.html";
+			}
 		}
 	}
 
@@ -77,6 +132,7 @@ public class MathController {
 		return sb.toString();
 	}
 
+	// 整数小数判定
 	public static boolean checkInt( String s ) {
 	    try {
 	    	Integer.parseInt( s );
