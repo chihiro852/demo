3d19213d3f3da44a90fa433406ace0aa30cc848d
chihiro.nakamura <chihiro.moment.1127@gmail.com>
2023-02-26 14:41:03 +0900

    FizzBuzz edit

--- src/main/java/com/example/demo/controller/FizzBuzzController.java
+++ src/main/java/com/example/demo/controller/FizzBuzzController.java
@@ -1,36 +1,38 @@
 package com.example.demo.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FizzBuzzController {
-	int fizz;
-	int buzz;
-	int fizzBuzz;
-	String all[];
 
-	
 	@GetMapping("/fizzBuzz")
 	public String doGet() {
 		return "fizzBuzz.html";
 	}
-	
+
 	@PostMapping("btn")
-	public String doPost( Model model) {
-		fizz = all();
-		
-		model.addAttribute("fizz", fizz);
+	public String doPost(@RequestParam("no") String no, Model model) {
+		try {
+			int n = Integer.parseInt(no);
+
+			for (int i = 1; i < n + 1; i++) {
+				if (i % 3 == 0 && i % 5 == 0) {
+					model.addAttribute("result", "FizzBuzz");
+				} else if (i % 3 == 0) {
+					model.addAttribute("result", "Fizz");
+				} else if (i % 5 == 0) {
+					model.addAttribute("result", "Buzz");
+				} else {
+					model.addAttribute("result", i);
+				}
+			}
+		} catch (NumberFormatException e) {
+			model.addAttribute("result", "数字を入力して下さい");
+		}
 		return "fizzBuzz.html";
-	}
-	
-	public int all() {
-		List<Integer> list = new ArrayList<>();
-		return fizz;
 	}
 }
