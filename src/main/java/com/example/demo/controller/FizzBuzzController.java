b05687e8b607d732369a24f06192073ce24d5eb2
chihiro.nakamura <chihiro.moment.1127@gmail.com>
2023-02-27 20:17:46 +0900

    edit fizzbuzz

--- src/main/java/com/example/demo/controller/FizzBuzzController.java
+++ src/main/java/com/example/demo/controller/FizzBuzzController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,20 +20,22 @@ public class FizzBuzzController {
 	public String doPost(@RequestParam("no") String no, Model model) {
 		try {
 			int n = Integer.parseInt(no);
+			ArrayList<String> list = new ArrayList<>();
 
 			for (int i = 1; i < n + 1; i++) {
-				if (i % 3 == 0 && i % 5 == 0) {
-					model.addAttribute("result", "FizzBuzz");
+				if (i % 15 == 0) {
+					list.add("FizzBuzz");
 				} else if (i % 3 == 0) {
-					model.addAttribute("result", "Fizz");
+					list.add("Fizz");
 				} else if (i % 5 == 0) {
-					model.addAttribute("result", "Buzz");
+					list.add("Buzz");
 				} else {
-					model.addAttribute("result", i);
+					list.add(String.valueOf(i));
 				}
 			}
+			model.addAttribute("result", list);
 		} catch (NumberFormatException e) {
-			model.addAttribute("result", "数字を入力して下さい");
+			model.addAttribute("result", "整数を入力して下さい");
 		}
 		return "fizzBuzz.html";
 	}
