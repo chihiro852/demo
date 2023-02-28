ee92746ee582613fae99e2284f03922a118ddd09
chihiro.nakamura <chihiro.moment.1127@gmail.com>
2023-03-01 01:25:34 +0900

    edit division

--- src/main/java/com/example/demo/Service/DivisionService.java
+++ src/main/java/com/example/demo/Service/DivisionService.java
@@ -5,14 +5,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class DivisionService {
 	public String dvsParam(String str1, String str2) {
-		int num;
+		int result;
 
 		try {
-			num = Integer.valueOf(str1) / Integer.valueOf(str2);
+			result = Integer.valueOf(str1) / Integer.valueOf(str2);
 		} catch (Exception e) {
 			throw e;
 		}
 
-		return String.valueOf(num);
+		return String.valueOf(result);
 	}
 }
